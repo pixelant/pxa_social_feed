b5e03208f52b09fb10d9d2f9ec41f890c13b6c23
Andriy Oprysko <pyvasyk@gmail.com>
2019-07-29 10:50:12 +0300

    [BUGFIX] fix description of plugin

--- Configuration/TSconfig/ContentElementWizard.ts
+++ Configuration/TSconfig/ContentElementWizard.ts
@@ -3,7 +3,7 @@ mod.wizards.newContentElement.wizardItems.plugins {
         pxasocialfeed {
             iconIdentifier = ext-pxasocialfeed-wizard-icon
             title = LLL:EXT:pxa_social_feed/Resources/Private/Language/locallang_be.xlf:mlang_tabs_tab
-            description = LLL:EXT:pxa_social_feed/Resources/Private/Language/locallang_be.xlf:pi1_wiz_description
+            description = LLL:EXT:pxa_social_feed/Resources/Private/Language/locallang_be.xlf:mlang_labels_tabdescr
             tt_content_defValues {
                 CType = list
                 list_type = pxasocialfeed_showfeed
