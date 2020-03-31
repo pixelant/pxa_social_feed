08701a6e0e951be42b94e2004bb2e79bedf6b0be
Andriy Oprysko <pyvasyk@gmail.com>
2020-03-31 16:49:47 +0300

    [TASK] use facebook graph version 6.0

--- Classes/GraphSdk/FacebookGraphSdkFactory.php
+++ Classes/GraphSdk/FacebookGraphSdkFactory.php
@@ -18,7 +18,7 @@ class FacebookGraphSdkFactory
      *
      * @var string
      */
-    protected static $version = 'v5.0';
+    protected static $version = 'v6.0';
 
     /**
      * Facebook factory
