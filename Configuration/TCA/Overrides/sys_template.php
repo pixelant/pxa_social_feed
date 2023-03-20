f2b704cb597ec18be02471f49325b10c03dd8096
Thomas Lüder <t.lueder@nets.de>
2023-03-20 22:47:33 +0100

    Split Ext template and convert file extensions

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,8 +1,14 @@
 <?php
+
 defined('TYPO3_MODE') or die();
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
     'pxa_social_feed',
     'Configuration/TypoScript',
-    'Pxa Social Feed'
+    'Pxa Social Feed Full'
+);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'pxa_social_feed',
+    'Configuration/TypoScript/Minimum',
+    'Pxa Social Feed Minimum (Without FE CSS & JS)'
 );
