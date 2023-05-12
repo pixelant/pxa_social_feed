f9c6b34e538b5c595ba9ae848c05298154d4b231
Michael Krohn <krohn@jakota.de>
2023-05-12 16:58:51 +0200

    [TASK] Initialize object storage

--- Classes/Domain/Model/Feed.php
+++ Classes/Domain/Model/Feed.php
@@ -151,6 +151,23 @@ class Feed extends AbstractEntity
      */
     protected $mediaType = self::IMAGE;
 
+    public function __construct()
+    {
+        // Do not remove the next line: It would break the functionality
+        $this->initializeObject();
+    }
+
+    /**
+     * Initializes all ObjectStorage properties when model is reconstructed from DB (where __construct is not called)
+     * Do not modify this method!
+     * It will be rewritten on each save in the extension builder
+     * You may modify the constructor of this class instead.
+     */
+    public function initializeObject(): void
+    {
+        $this->falMedia = $this->falMedia ?? new ObjectStorage();
+    }
+
     /**
      * Returns the date
      *
